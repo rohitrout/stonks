1379946fb00aa6a43adefce1c43acf85f8a08464
Rohit Rout <rohitrout@outlook.com>
2021-09-30 12:58:01 +0530

    html done

---
+++ app.js
@@ -0,0 +1,10 @@
+var initPrice=document.querySelector("#initial-price");
+var curPrice=document.querySelector("#current-price");
+var quant=document.querySelector("#quantity");
+
+var calc=document.querySelector("#calc");
+
+calc.addEventListener("click", function calculate(){
+   
+
+})
