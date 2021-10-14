8e186d91059f7cc783bed3cbb63127076c6b0ddb
Rohit Rout <rohitrout@outlook.com>
2021-10-14 11:53:58 +0530

    updated absolute Amount

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var p1 = document.querySelector("#initial-price");
 var q1 = document.querySelector("#quantity");
 var p2 = document.querySelector("#current-price");
 var btnCalc = document.querySelector("#btn-calc");
+var absVal=document.querySelector("#output1");
 
 btnCalc.addEventListener("click", function calculate() {
   if (p1.value * q1.value > p2.value * q1.value) {
@@ -10,14 +11,16 @@ btnCalc.addEventListener("click", function calculate() {
     var l = li - lc;
     var loss = (l * 100) / li;
     console.log("loss", loss);
-    document.getElementById("output").innerHTML = "LOSS " + loss + "%";
+    absVal.innerText="Loss Amount : "+l;
+    document.getElementById("output").innerHTML = "LOSS :" + loss + "%";
   } else {
     var pi = p1.value * q1.value;
     var pc = p2.value * q1.value;
     var p = pc - pi;
     var profit = (p * 100) / pi;
     console.log("profit", profit);
-    document.getElementById("output").innerHTML = "PROFIT " + profit + "%";
+    absVal.innerText="Profit Amount : "+p;
+    document.getElementById("output").innerHTML = "PROFIT :" + profit + "%";
   }
   // console.log(p1.value * q1.value);
   // console.log(p2.value);
