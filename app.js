5b0fbb4a649f5c0c509548bc971f87878ddaef22
Rohit Rout <rohitrout@outlook.com>
2021-11-23 10:33:34 +0530

    update to handle negative values

--- app.js
+++ app.js
@@ -5,22 +5,26 @@ var btnCalc = document.querySelector("#btn-calc");
 var absVal=document.querySelector("#output1");
 
 btnCalc.addEventListener("click", function calculate() {
-  if (p1.value * q1.value > p2.value * q1.value) {
-    var li = p1.value * q1.value;
-    var lc = p2.value * q1.value;
-    var l = li - lc;
-    var loss = (l * 100) / li;
-    console.log("loss", loss);
-    absVal.innerText="Loss Amount : "+l;
-    document.getElementById("output").innerHTML = "LOSS :" + loss + "%";
+  if (Number(p1.value) <= 0 || Number(q1.value) <= 0 || Number(p2.value) <= 0) {
+    absVal.innerHTML = "Invalid Input";
   } else {
-    var pi = p1.value * q1.value;
-    var pc = p2.value * q1.value;
-    var p = pc - pi;
-    var profit = (p * 100) / pi;
-    console.log("profit", profit);
-    absVal.innerText="Profit Amount : "+p;
-    document.getElementById("output").innerHTML = "PROFIT :" + profit + "%";
+    if (p1.value * q1.value > p2.value * q1.value) {
+      var li = p1.value * q1.value;
+      var lc = p2.value * q1.value;
+      var l = li - lc;
+      var loss = (l * 100) / li;
+      console.log("loss", loss);
+      absVal.innerText = "Loss Amount : " + l;
+      document.getElementById("output").innerHTML = "LOSS :" + loss + "%";
+    } else if (p1.value * q1.value < p2.value * q1.value) {
+      var pi = p1.value * q1.value;
+      var pc = p2.value * q1.value;
+      var p = pc - pi;
+      var profit = (p * 100) / pi;
+      console.log("profit", profit);
+      absVal.innerText = "Profit Amount : " + p;
+      document.getElementById("output").innerHTML = "PROFIT :" + profit + "%";
+    }
   }
   // console.log(p1.value * q1.value);
   // console.log(p2.value);
